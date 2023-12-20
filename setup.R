cdd4d44c38c14b96b4e467c82562f22f82ccb9e5
Till Hoffmann <tillahoffmann@gmail.com>
2023-12-20 16:29:05 +0100

    Install gptoolsStan from CRAN.

--- setup.R
+++ setup.R
@@ -1,3 +1,3 @@
 repos <- c("https://mc-stan.org/r-packages/", "http://cran.us.r-project.org")
 install.packages("cmdstanr", repos=repos)
-devtools::install_github("onnela-lab/gptoolsStan")
+install.packages("onnela-lab/gptoolsStan", repos=repos)
